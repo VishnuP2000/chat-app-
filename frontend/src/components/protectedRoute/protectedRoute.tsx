10331aae12f319d35436b1f5e6cd69a84cd7faaf
vishnu p <vishnuJithu60@gmail.com>
2026-08-24 21:22:00 +0530

    add log

--- frontend/src/components/protectedRoute/protectedRoute.tsx
+++ frontend/src/components/protectedRoute/protectedRoute.tsx
@@ -5,9 +5,12 @@ interface Props {
 }
 
 export default function ProtectedRoute({ children }: Props) {
+  console.log('protected route 1')
   const token = localStorage.getItem("accessToken");
-
+  console.log('protected route 2',token)
+  
   if (!token) {
+    console.log('token is lost in protectedRout')
     return <Navigate to="/sign-in" replace />;
   }
 
