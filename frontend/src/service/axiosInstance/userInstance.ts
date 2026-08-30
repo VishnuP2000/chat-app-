2b4e0c5414e91263eb74fd7a4819327cecb97f1d
vishnu p <vishnuJithu60@gmail.com>
2026-08-30 13:57:29 +0530

    change clg

--- frontend/src/service/axiosInstance/userInstance.ts
+++ frontend/src/service/axiosInstance/userInstance.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 
 const API_URL = import.meta.env.USER_BASE_URL;
+console.log('API_URL 1',API_URL)
 
 // ---------------- Public instance (no access token needed) ----------------
 export const publicAxios = axios.create({
